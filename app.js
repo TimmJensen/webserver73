ba05271b7c65285b8ee072d22e8ee46e154928fd
Timm Jensen <4342842@rts-365.dk>
2017-11-29 11:20:48 +0100

    Change port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const restify = require('restify');
 const path = require('path');
 
-const port = process.env.port || 5000;
+const port = process.env.port || 80;
 
 const app = restify.createServer({
     name: 'webserverTest',
